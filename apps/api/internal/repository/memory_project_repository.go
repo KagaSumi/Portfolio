dba171719ca922a6156e6b6682867dbe8a8a084c
Quinten Leung <wleung105@my.bcit.ca>
2026-05-23 15:45:02 -0700

    Add create and error to memory repo

--- apps/api/internal/repository/memory_project_repository.go
+++ apps/api/internal/repository/memory_project_repository.go
@@ -4,14 +4,17 @@ import "github.com/KagaSumi/Portfolio/apps/api/internal/model"
 
 type MemoryProjectRepo struct{}
 
-func (r MemoryProjectRepo) GetAll() []model.Project {
-	return []model.Project{
-		{
-			Title:       "Relics and Receipts",
-			Description: "2D shop management game in C++ SDL3",
-			Stack:       []string{"C++", "SDL3"},
-			Github:      "https://github.com/KagaSumi/Relics_and_Receipts",
-		},
-	}
+func (r MemoryProjectRepo) GetAll() ([]model.Project, error) {
+    return []model.Project{
+        {
+            Title:       "Relics and Receipts",
+            Description: "2D shop management game in C++ SDL3",
+            Stack:       []string{"C++", "SDL3"},
+            Github:      "https://github.com/KagaSumi/Relics_and_Receipts",
+        },
+    }, nil
 }
 
+func (r MemoryProjectRepo) Create(p model.Project) error {
+    return nil
+}
